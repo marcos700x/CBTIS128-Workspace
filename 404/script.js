2ae5e42b23b67deaf3e73675b822aea0704e00a1
Marcos Casas Caldera <85005420+marcos700x@users.noreply.github.com>
2022-01-17 15:28:05 -0700

    404 arreglado

--- 404/script.js
+++
@@ -1,5 +0,0 @@
-var contenedor__frase = document.querySelector(".subtitulo__404");
-var frases = ["¡Te juro que un perro se comío esta pagina!", "¡¿La pagína tenía que estar lista para hoy?!", "La cafetería esta cerrada, vuelve en otra ocasión.", "¿Dónde deje la pagina? ¡Debería estar en mi mochila!","Profe, ¿Puedo entregar la pagina mañana?","Puedes ir a la cafetería mientras buscamos la pagina."];
-var lenghtFrases = frases.length;
-var randonNumber = Math.ceil(Math.random() * lenghtFrases) - 1 ;
-contenedor__frase.innerHTML = frases[randonNumber];
